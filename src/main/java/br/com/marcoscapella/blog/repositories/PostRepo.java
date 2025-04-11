47603c788a48c853e34382e7fcbe70370254937d
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-04-11 10:43:38 -0300

    Fixing lazy response for tags.

--- src/main/java/br/com/marcoscapella/blog/repositories/PostRepo.java
+++ src/main/java/br/com/marcoscapella/blog/repositories/PostRepo.java
@@ -2,9 +2,14 @@ package br.com.marcoscapella.blog.repositories;
 
 import br.com.marcoscapella.blog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PostRepo extends JpaRepository<Post, Long> {
     Optional<Post> findBySlug(String slug);
+
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.tags WHERE p.id = :id")
+    Optional<Post> findByIdWithTags(@Param("id") Long id);
 }
